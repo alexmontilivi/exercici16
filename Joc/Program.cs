845558b8352c293493e36f90dc6d346ae5738fa6
Alex <alex.tetsu11@gmail.com>
2019-10-01 16:54:45 +0200

    he afegit el joc

---
+++ Joc/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Joc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcio;
+            Console.WriteLine("Introdueix el numero del joc!");
+            Console.WriteLine();
+            Console.WriteLine("***** 1 - CS - *****");
+            Console.WriteLine("***** 2 - TETRIS     - *****");
+            Console.WriteLine("***** 3 - SALIR      - *****");
+            opcio = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            switch (opcio)
+            {
+                case 1:
+                    Console.WriteLine("HOLA CMAN!");
+                    break;
+
+                case 2:
+                    Console.WriteLine("HOLA AMANTE DEl TETRIS!");
+                    break;
+
+                case 3:
+                    Console.WriteLine("HASTA LUEGO MARICARMEN!");
+                    break;
+            }
+        }
+    }
+}
